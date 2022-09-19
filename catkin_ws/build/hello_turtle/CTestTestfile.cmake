11677311bd7a26423cbb67a434105986f3f7954a
allan <aserrato@unal.edu.co>
2022-09-19 18:52:41 -0500

    actualizacion carpeta ROS
    se mete la carpeta catkin con los scrpits de python

--- catkin_ws/build/hello_turtle/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/allan/catkin_ws/src/hello_turtle
-# Build directory: /home/allan/catkin_ws/build/hello_turtle
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
